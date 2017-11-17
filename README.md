4fc826e3a9b90b431deb95c98c04e059fb3cbef7
Shreyansh Zazz <shrey.binary@gmail.com>
2017-11-17 19:44:10 +0530

    Add Header
    Add Header Section in Homepage#main-stage.
    
    - Complete Responsive
    - No Bug till this commit.

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # TechFusion Club Website
 
-<img src="https://badges.frapsoft.com/os/v2/open-source.png?v=103" width="140" /> <img src="https://badges.frapsoft.com/os/mit/mit.svg?v=103" width="140" /> <img src="http://forthebadge.com/images/badges/built-with-love.svg" width="140" />
-
-![portfolio-design](./portfolio-design.png)
-
 ---
 
 ## [Live website here](https://techfusionclub.github.io/)
--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,1 +1,20 @@
-console.log("Hello from TechFusion");
+//@prepros-prepend velocity.js
+
+var tfNav = document.getElementById("tf-nav");
+var tfMobNav = document.getElementById("tf-mob-nav");
+var tfNavLinks = document.getElementsByClassName("tf-nav-link");
+var flag = false;
+
+tfMobNav.addEventListener('click', function() {
+ if(flag){
+  tfMobNav.classList.remove("active");
+  Velocity(tfNav, { opacity: 0 }, { display: "none" });
+  Velocity(tfNavLinks, "slideUp", { delay: 50, duration: 400 });
+  flag = false;
+ } else{
+  tfMobNav.classList.add("active");
+  Velocity(tfNav, { opacity: 1 }, { display: "flex" });
+  Velocity(tfNavLinks, "slideDown", { delay: 50, duration: 400 });
+  flag = true;
+ }
+});
